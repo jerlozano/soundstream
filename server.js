63b75d305869f1d621557407a3321c71afc3160f
Jeremy Lozano <jeremy.lozano@connectsolutions.com>
2014-10-22 16:49:34 -0700

    allow port to be dynamically set

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./index');
 var logger = require('./common/logger');
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   logger.info('Server started on 0.0.0.0:3000');
 });
