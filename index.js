54cf85d469eb344c906e2898d9eb66f4364ec162
Andrew Baldock <andrewbaldock3@gmail.com>
2014-10-22 10:29:38 -0700

    little express server with http response

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var http = require('http');
+
 var app = express();
 
-app.route('/', function(req, res){
-  res.send('hello');
-});
 
-app.listen(3000);
+// app.route('/', function(req, res){
+//   res.send('hello');
+// });
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<html><head></head><body>HELLO</body></html>');
+  response.end();
+}).listen(3000);
