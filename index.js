06dfbaccb162c06770fb92d9f8b18f92145219c8
Andrew Baldock <andrewbaldock3@gmail.com>
2014-10-23 09:06:02 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 var express = require('express');
-var http   = require('http');
-var app    = express();
-var Search = require('./controllers/search');
-var logger = require('./common/logger');
-var path   = require('path');
+var logger  = require('./common/logger');
+var path    = require('path');
+var Search  = require('./controllers/search');
+
+var app = express();
 
 var search = new Search();
 search.initialize();
 
 app.get('/', function (req, res) {
-
   res.send('app is running');
-
 });
 
 app.get('/search/track/id/:id', function (req, res) {
-
   var id = req.params.id;
   logger.info('searching for a track by id: ' + id);
   search.getTrackById(id, res);
-
 });
 
 app.get('/search/keyword/:keyword', function (req, res) {
-
   var keyword = req.params.keyword;
   logger.info('searching for a track by keyword: ' + keyword);
   search.getTrackByKeyword(keyword, res);
-
 });
 
 app.get('/home', function(req,res) {
