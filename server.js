8ddc81f51e24519be74ffba71a0b8258e7900484
Jeremy Lozano <jeremy.lozano@connectsolutions.com>
2014-10-23 10:47:52 -0700

    cleaning up port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('./index');
 var logger = require('./common/logger');
 
-app.listen(process.env.PORT || 3000, function() {
-  logger.info('Server started on 0.0.0.0:3000');
+var envPort = process.env.PORT || 3000;
+
+app.listen(envPort, function() {
+  logger.info('Server started on 0.0.0.0:' + envPort);
 });
