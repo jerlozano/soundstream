410b816d0411d7ed4bfd64ee957a0f75ea4cf53a
Jeremy Lozano <jeremy.lozano@connectsolutions.com>
2014-10-23 10:15:40 -0700

    added logging to disk

--- common/logger.js
+++ common/logger.js
@@ -3,7 +3,8 @@ var config  = require('config');
 
 var winston = new (winston.Logger)({
     transports: [
-        new (winston.transports.Console)({ level: config.consoleLoggingLevel })
+        new (winston.transports.Console)({ level: config.consoleLoggingLevel }),
+        new (winston.transports.File)({ filename: './app-logging.log', level: config.fileLoggingLevel })
     ]
 });
 
